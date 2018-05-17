739bfee48a5dbc651920a56f2c283c2e168089cf
Eunjong Kim <eunjong@live.com>
2018-05-17 12:13:08 +0900

    version 0.1.4

--- Automatic_Model_Selection.py
+++ Automatic_Model_Selection.py
@@ -3,8 +3,8 @@
 # Title          : Automatic Model Selection
 # Author         : 김은종
 # Origin_Date    : 05/11/2018
-# Revision_Date  : 05/14/2018
-# Version        : '0.1.2'
+# Revision_Date  : 05/17/2018
+# Version        : '0.1.4'
 
 
 import h2o
@@ -17,8 +17,8 @@ df = h2o.import_file(path="./Test/creditcard.csv", destination_frame="df")
 print(df.head())
 
 # Input parameters that are going to train (Target)
-response_column = 'Amount'
-# response_column = 'Class'
+# response_column = 'Amount'
+response_column = 'Class'
 
 print(response_column)
 
@@ -28,14 +28,17 @@ print(training_columns)
 # TODO: from h2o.estimators.deeplearning import H2OAutoEncoderEstimator (H2OAutoEncoder)
 
 # Split data into train and testing
-train, test = df.split_frame(ratios=[0.8])
-
+train, test = df.split_frame(ratios=[0.7])
 
 # For regression     --- default
 # For classification --- binary
 
-response_type = input("변수 형태를 입력해주세요. (default/binary): ")
-# TODO: try-catch
+while True:
+    try:
+        response_type = input("변수 형태를 입력해주세요. (default/binary): ")
+        break
+    except TypeError:
+        print("입력한 값은 문자열이 아닙니다. 다시 입력해주세요,")
 
 # For binary classification, response should be a factor
 if response_type == 'binary':
@@ -45,145 +48,52 @@ if response_type == 'binary':
 ### AutoML
 
 # Time to run the experiment
-run_automl_for_seconds = int(input("최대 허용 시간을 입력해주세요. (s): "))
-# TODO: try-catch
+while True:
+    try:
+        run_automl_time = int(input("최대 허용 시간을 입력해주세요. (s): "))
+        break
+    except ValueError:
+        print("입력한 값은 정수가 아닙니다. 다시 입력해주세요.")
 
 # RUN AutoML
-aml = H2OAutoML(max_runtime_secs=run_automl_for_seconds)
+aml = H2OAutoML(max_runtime_secs=run_automl_time)
 
 aml.train(x=training_columns, y=response_column,
           training_frame=train,
           leaderboard_frame=test)
 
 # View the AutoML Leaderboard
+
 lb = aml.leaderboard
-print(lb)
 
-# The leader model is stored here
-aml.leader
-print(aml.leader)
+# Get Model Ids
+model_ids = list(lb['model_id'].as_data_frame().iloc[:, 0])
+model_number = h2o.H2OFrame(list(range(1,len(model_ids)+1)),column_names=[' '])
 
-# predict
-pred = aml.predict(test_data=test)
+print(model_number.cbind(lb))
 
-pred_df = pred.as_data_frame()
-pred_df.to_csv('./Test/creditcard_result.csv', header=True, index=False)
+# Model Selection (input)
+while True:
+    try:
+        model_number = int(input("원하는 모델 번호를 입력하세요.: "))
+        selected_model = h2o.get_model(model_ids[model_number-1])
+        print(selected_model)
+        break
+    except ValueError:
+        print("입력한 값은 정수가 아닙니다. 다시 입력해주세요.")
 
+while True:
+    key_input = input("예측 모델과 결과 값을 저장하시겠습니까? (y/n): ")
 
-
-
-""" for version 0.2.1 (Automatic Feature Selection)
-
-
-if data==numerical:
-    Do nothing
-elif data==categorical:
-    Label encoding
-    one-hot encoding
-elif data==text:
-    counts
-    tf-idf
-
-# Label encoding
-# from sklearn.preprocessing import LabelEncoder
-#
-# lbl_enc = LabelEncoder()
-# lbl_enc.fit(xtrain[categorical_features])
-# xtrain_cat = lbl_enc.transform(xtrain[categorical_features])
-#
-# One-hot encoding
-# from sklearn.preprocessing import OneHotEncoder
-#
-# ohe = OneHotEncoder()
-# ohe.fit(xtrain[categorical_features])
-# xtrain_cat = ohe.transform(xtrain[categorical_features])
-
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-Tfv = TfidfVectorizer(min_df=3, max_features=None,
-                      strip_accents='unicode', analyzer='word', token_pattern=r'\w{1,',
-                      ngram_range=(1, 2), use_idf=1, smooth_idf=1, sublinear_tf=1,
-                      stop_words='english')
-
-import numpy as np
-from scipy import sparse
-
-# In case of dense data
-X = np.hstack([x1, x2, x3])
-
-# In case data is sparse
-X = sparse.hstack([x1, x2, x3])
-
-from sklearn.pipeline import FeatureUnion
-from sklearn.decomposition import PCA
-from sklearn.feature_selection import SelectKBest
-
-pca = PCA(n_compoents=10)
-skb = SelectKBest(k=1)
-combined_features = FeatureUnion([('pca', pca), ('skb', kskb)])
-
-pca = PCA(n_components=12)
-pca.fit(xtrain)
-xtrain = pca.transform(xtrain)
-
-from sklearn.decomposition import TruncatedSVD
-
-svd = TruncatedSVD(n_components=128)
-svd.fit(xtrain)
-xtrain = svd.transform(xtrain)
-# Random Forest Feature Selection
-from sklearn.ensemble import RandomForestClassifier
-
-clf = RandomForestClassifier(n_estimators=100, n_jobs=-1)
-clf.fit(X, y)
-X_selected = clf.transform(X)
-
-# Feature importances from GBM (XGBOOST)
-import xgboost as xgb
-
-params = {}
-
-model = xgb.train(params, xtrain, num_boost_round=100)
-sorted(model.get_fscore().items(), key=lambda t: -t[1])
-
-# Chi2 feature election
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import chi2
-
-skb = SelectKBest(chi2, k=20)
-skb.fit_transform(X, y)
-
-
-# Greedy feature selection
-
-def selectionLoop(self, X, y):
-    score_history = []
-    good_features = set([])
-    num_features = X.shape[1]
-
-    while len(score_history) < 2 or score_history[-1][0] > score_history[-2][0]:
-        scores = []
-        for feature in range(num_features):
-            if feature not in good_features:
-                selected_features = list(good_features) + [feature]
-
-                Xts = np.column_stack(X[:, j] for j in selected_features)
-
-                score = self.evaluateScore(Xts, y)
-                scores.append((score, feature))
-
-                if self._verbose:
-                    print('Current AUC :', np.mean(score))
-
-            good_features.add(sorted(scores)[-1][1])
-            score_history.append(sorted(scores)[-1])
-            if self._verbose:
-                print('Current Features : ', sorted(list(good_features)))
-
-        # Remove last added feature
-        good_features.remove(score_history[-1][1])
-        good_features = sorted(list(good_features))
-        if self._verbose:
-            print('Selected Features : ', good_features)
-"""
+    if key_input.upper() == 'Y':
+        pred = selected_model.predict(test_data=test)
+        pred_df = pred.as_data_frame()
+        pred_df.to_csv('./Test/creditcard_result.csv', header=True, index=False)
+        h2o.save_model(selected_model, path="./product_backorders_model_bin")
+        print("저장되었습니다.")
+        break
+    elif key_input.upper() == 'N':
+        print("저장되지 않았습니다.")
+        break
+    else:
+        print("잘못 입력하셨습니다. 다시 입력해주세요.")
